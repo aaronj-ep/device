d4dc9b6f6042379a0c524ff2832cdc715fdaa3af
AMJones <aaronj@jonesiscoding.com>
2018-04-02 08:53:19 -0500

    Rewritten detection Javascript.

--- src/js/xq-detect.js
+++ src/js/xq-detect.js
@@ -3,209 +3,144 @@
  * @author  Aaron M Jones [am@jonesiscoding.com]
  * @licence MIT (https://github.com/exactquery/xq-detect/blob/master/LICENSE)
  */
-(function() {
-    // Define our constructor
-    this.Detect = function() {
-        // Set Needed variables
-        this.Conn = navigator.connection || navigator.mozConnection || navigator.webkitConnection || false;
-        this.CookieName = (typeof arguments[0] !== "undefined") ? arguments[0] : "djs";
-
-        // Media Query PolyFill
-        if (!hasMediaQuery()) {
-            addMediaQuery();
-        }
-
-        // Set Detection Variables
-        this.Width = screen.width;
-        this.Height = screen.height;
-        this.Browser = (this.isBrowserBaseline()) ? 'baseline' : (this.isBrowserFallback()) ? 'fallback' : 'modern';
-    };
-
-    /**
-     * Detects if a device is running Android.  IMPORTANT NOTE: This uses the UserAgent, and therefore can be spoofed.
-     * It should be used for aesthetics only.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isDeviceAndroid = function() {
-        return true === (/(android)/i.test(navigator.userAgent));
-    };
-
-    /**
-     * Detects if a device is running iOS.  IMPORTANT NOTE: This uses the UserAgent, and therefore can be spoofed.
-     * It should be used for aesthetics only.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isDeviceIOS = function() {
-        return true === (/(ipod|iphone|ipad)/i.test(navigator.userAgent));
-    };
-
-    /**
-     * Determines if a browser is 'baseline', based on the detection of specific CSS3 functionality.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isBrowserBaseline = function() {
-      return true === (!('flexBasis' in document.documentElement.style
-        || 'msFlexPreferredSize' in document.documentElement.style
-        || 'WebkitFlexBasis' in document.documentElement.style));
-    };
-
-    /**
-     * Determines if a browser is 'fallback', based on the detection of specific Media Query 4 functionality.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isBrowserFallback = function() {
-        return true === (!hasMediaQuery() || !(xMatchMedia('(pointer:fine')
-        || xMatchMedia('(pointer:coarse)')
-        || xMatchMedia('(-moz-touch-enabled')))
-    };
-
-    /**
-     * Determines if a HiDPI screen is being used, such as an Apple Retina display.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isHighResDevice = function() {
-        // Primary method, as this doesn't fall victim to issues with zooming.
-        var testQuery = '(-webkit-min-device-pixel-ratio: 1.0), (min-resolution: 96dpi), (min-resolution: 1dppx)';
-        if (hasMediaQuery() && xMatchMedia(testQuery)) {
-            var mediaQuery = '(-webkit-min-device-pixel-ratio: 1.5), (min-resolution: 144dpi), (min-resolution: 1.5dppx)';
-            return xMatchMedia(mediaQuery);
-        }
-
-        // Fallback for older versions & mobile versions of IE
-        var deviceXDPI = (typeof window.screen.deviceXDPI !== 'undefined') ? window.screen.deviceXDPI : null;
-        var logicalXDPI = (typeof window.screen.logicalXPDI !== 'undefined') ? window.screen.logicalXPDI : null;
-        if (deviceXDPI && logicalXDPI) {
-            return true === ((deviceXDPI / logicalXDPI) > 1.5);
-        }
-
-        // Final fallback, which WILL report HiDPI if the window is zoomed.
-        var devicePixelRatio = window.devicePixelRatio || 1;
-        return true === (devicePixelRatio > 1.5);
-    };
-
-    /**
-     * Detects if a device is reporting that it uses a metered connection.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isMetered = function() {
-        return true === (this.Conn && this.Conn.metered);
-    };
-
-    /**
-     * Detects if a device has a touch screen.
-     *
-     * @returns {boolean}
-     */
-    Detect.prototype.isTouchDevice = function() {
-        if (hasMediaQuery() && (xMatchMedia('(pointer:coarse)') || xMatchMedia('(-moz-touch-enabled)'))) {
-            return true;
-        }
-        if ("ontouchstart" in window) {
-            return true;
-        }
-        if ((navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0)) {
-            return true;
-        }
-
-        return true === (/(touch)/i.test(navigator.userAgent));
-    };
-
-    Detect.prototype.setCookie = function() {
-
-        var cookieValue = '{ "width": "' + this.Width
-            + '", "height": "' + this.Height
-            + '", "browser": "' + this.Browser
-            + '", "hidpi": "' + this.isHighResDevice()
-            + '", "metered": "' + this.isMetered()
-            + '", "touch": "' + this.isTouchDevice()
-            + '", "android": "' + this.isDeviceAndroid()
-            + '", "ios": "' + this.isDeviceIOS() + '" }';
-
-        document.cookie = this.CookieName+"="+encodeURIComponent(cookieValue)
-            + ";path=/";
-    };
-
-    Detect.prototype.setTags = function() {
-
-        var d = document.documentElement;
-
-        // Replace the no-js tag, because if we're running, we have JavaScript.
-        d.className = d.className.replace('no-js', 'js');
-        // Replace the 'fallback' browser tag, which is only there in case of no-js.
-        d.className = d.className.replace(' fallback', '');
-
-        // Other HTML Tag Changes
-        var addTag = '';
-        if (this.Browser !== 'modern') {
-            addTag += " " + this.Browser;
-        }
-        if(this.isTouchDevice()) {
-            addTag += " touch";
-            if (this.isDeviceAndroid()) {
-                addTag += " android";
-            }
-            if (this.isDeviceIOS()) {
-                addTag += " ios";
-            }
-        }
-
-        d.className += addTag;
-        d.setAttribute('data-user-agent', navigator.userAgent);
-    };
-
-    /**
-     * Private method for performing a media query cross platforms & specifications.  Use of this method should always
-     * be paired with hasMediaQuery() to prevent false positives.
-     *
-     * @param   media       A media query
-     * @returns {boolean|null}
-     */
-    function xMatchMedia(media) {
-        if (typeof window.matchMedia !== "undefined") {
-            return window.matchMedia(media).matches;
-        }
-
-        return null;
+var detect = function (w, d) {
+  'use strict';
+  var mm    = w.matchMedia || w.webkitMatchMedia || w.mozMatchMedia || w.oMatchMedia || w.msMatchMedia || false;
+  var de    = d.documentElement;
+  var nav   = navigator;
+  var _dt   = { width: screen.width, height: screen.height };
+  
+  // HELPER FUNCTIONS
+  function add(name, func) {
+    if (!(name in _dt) && typeof func === "function") {
+      _dt[name] = func;
     }
-
-    /**
-     * Private method for detecting if the device is capable of using media queries.
-     *
-     * @returns {boolean}
-     */
-    function hasMediaQuery() {
-        return (typeof window.matchMedia !== "undefined");
-    }
-
-    /**
-     * Polyfill for window.matchMedia on IE9/10 and older versions of webkit.
-     */
-    function addMediaQuery() {
-        var mqPoly;
-        if (mqPoly = (window.webkitMatchMedia || window.mozMatchMedia || window.oMatchMedia || window.msMatchMedia)) {
-            window.matchMedia = mqPoly;
+    
+    return _dt;
+  }
+  
+  function mq(q) {
+    return true === (mm && mm(q));
+  }
+  
+  function save(tests, cookieName) {
+    var recipe = {};
+    var cName = cookieName || 'djs';
+    for (var key in tests) {
+      if (tests.hasOwnProperty(key) && (key in _dt)) {
+        var args = ('object' === typeof tests[key]) ? tests[key] : [tests[key]];
+        recipe[key] = _dt[key](args);
+        if (recipe[key]) {
+          de.classList.add(key);
         } else {
-            if (mqPoly = (window.styleMedia || window.media)) {
-                (window.matchMedia = function () {
-                    "use strict";
-                    return function (media) {
-                        return {
-                            matches: mqPoly.matchMedium(media || 'all'),
-                            media: media || 'all'
-                        };
-                    };
-                });
-            }
+          de.classList.remove(key);
         }
+      }
     }
-}());
-
-var detect = new Detect();
-detect.setTags();
-detect.setCookie();
+    de.className = de.className.replace('no-js', 'js');
+    document.cookie = cName + '=' + JSON.stringify(recipe) + ';path=/';
+  }
+  
+  function ua(arg) {
+    var pattern = ( arg instanceof RegExp ) ? arg : new RegExp('/(' + arg + ')/i');
+    
+    return true === ( pattern.test( navigator.userAgent ) );
+  }
+  
+  // TEST FUNCTIONS
+  function getScrollbar() {
+    var sb = d.getElementById( 'xqsbM' ) ||
+      ( function () {
+        var sbel = '<div style="width:100px;overflow:scroll;position:absolute;top:-9999px;"><div id="xqsbM" style="margin-right:calc(100px - 100%);"></div></div>';
+        d.body.insertAdjacentHTML( 'beforeend', sbel );
+        return d.getElementById( 'xqsbM' );
+      } )();
+    
+    return getComputedStyle( sb ).marginRight;
+  }
+  
+  function isBaseline() {
+    return true === (!('localStorage' in w && mm && 'opacity' in de.style && 'borderRadius' in de.style));
+  }
+  
+  function isBreakpoint(points) {
+    var query = w.getComputedStyle(d.querySelector('body'), ':before').getPropertyValue('content').replace(/"/g, '') || null;
+    if ( !Array === points.constructor ) { points = [ points ]; }
+    
+    return (null !== query) ? (points.indexOf(query) !== -1) : null;
+  }
+  
+  function isFallback() {
+    return true === (!('flexBasis' in de.style || 'msFlexPreferredSize' in de.style || 'WebkitFlexBasis' in de.style));
+  }
+  
+  /**
+   * Determines if a HiDPI screen is being used, such as an Apple Retina display.
+   *
+   * @returns {boolean}
+   */
+  function isHighRes(tRatio) {
+    var ratio = tRatio || 1.5;
+    var minRes = ratio * 96;
+    
+    // Primary method, as this doesn't fall victim to issues with zooming.
+    var testQuery = '(-webkit-min-device-pixel-ratio: 1.0), (min-resolution: 96dpi), (min-resolution: 1dppx)';
+    if ( mq( testQuery ) ) {
+      var mediaQuery = '(-webkit-min-device-pixel-ratio: ' + ratio + '), (min-resolution: ' + minRes + 'dpi), (min-resolution: ' + ratio + 'dppx)';
+      return mq( mediaQuery );
+    }
+    
+    // Fallback for older versions & mobile versions of IE
+    var deviceXDPI = ( typeof w.screen.deviceXDPI !== 'undefined' ) ? w.screen.deviceXDPI : null;
+    var logicalXDPI = ( typeof w.screen.logicalXPDI !== 'undefined' ) ? w.screen.logicalXPDI : null;
+    if ( deviceXDPI && logicalXDPI ) {
+      return true === ( ( deviceXDPI / logicalXDPI ) > ratio );
+    }
+    
+    // Final fallback, which WILL report HiDPI if the window is zoomed.
+    var devicePixelRatio = w.devicePixelRatio || 1;
+    return true === ( devicePixelRatio > ratio );
+  }
+  
+  function isMetered() {
+    var conn = nav.connection || nav.mozConnection || nav.webkitConnection || false;
+    
+    return true === ( conn && conn.metered );
+  }
+  
+  function isModern() {
+    return true === !( isBaseline() || isFallback() );
+  }
+  
+  /**
+   * Detects if a device has a touch screen.
+   *
+   * @returns {boolean}
+   */
+  function isTouch() {
+    if ( mq( '(pointer:coarse)' ) || mq( '(-moz-touch-enabled)' ) ) { return true; }
+    if ( "ontouchstart" in w ) { return true; }
+    if ( ( nav.maxTouchPoints > 0 || nav.msMaxTouchPoints > 0 ) ) { return true; }
+    
+    return true === ua('touch');
+  }
+  
+  _dt.android    = ua('android');
+  _dt.browser    = (isModern()) ? 'modern' : (isFallback()) ? 'fallback' : 'baseline';
+  _dt.ios        = ua('iphone|ipod|ipad');
+  _dt.add        = add;
+  _dt.baseline   = isBaseline;
+  _dt.breakpoint = isBreakpoint;
+  _dt.fallback   = isFallback;
+  _dt.modern     = isModern;
+  _dt.highres    = isHighRes;
+  _dt.hidpi      = isHighRes;
+  _dt.metered    = isMetered;
+  _dt.retina     = isHighRes;
+  _dt.save       = save;
+  _dt.scrollbar  = getScrollbar;
+  _dt.touch      = isTouch;
+  
+  return _dt;
+  
+}(window, document);
