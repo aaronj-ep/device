22a37ae5bb42f9b192124bf208249e1493552149
AMJones <am@jonesiscoding.com>
2022-06-27 13:09:53 -0500

    Strips outer quotes when returning header values.

--- src/Helper/Resolver/HeaderBag.php
+++ src/Helper/Resolver/HeaderBag.php
@@ -7,6 +7,16 @@ use DevCoding\Client\Object\Headers\HeaderBag as BaseHeaderBag;
 
 class HeaderBag extends BaseHeaderBag
 {
+  public function get(string $id)
+  {
+    if ($value = parent::get($id))
+    {
+      return preg_replace('#(?:"([^,]*)")#', '$1', $value);
+    }
+
+    return null;
+  }
+
   /**
    * @return bool
    */
